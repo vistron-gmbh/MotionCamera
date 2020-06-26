656248a3f108bd1a169562c2240defe33d1f7d5a
Paul Wenzlaff <Paulwenzlaff@gmail.com>
2020-06-26 10:21:06 +0200

    Telegram added

--- MotionCameraClient/MotionCameraClient.py
+++ MotionCameraClient/MotionCameraClient.py
@@ -1,1 +1,46 @@
-print("Hello");
+#Libraries
+from gpiozero import MotionSensor
+from picamera import PiCamera
+from time import sleep
+import telebot
+
+#Declaration
+camera = PiCamera()
+pir = MotionSensor(16)
+cameraOnline = False
+led = LED(17)
+bot = telebot.TeleBot("886187441:AAFmuBkGdv4bDJHYaDQVXFWaePyYQic6Eko")
+
+#Settings
+camera.roatation = 180
+camera.start_preview()
+# camera.stop_preview()
+
+def ToggleBot(value):
+    if value == True:
+        cameraOnline = True
+        print("Cameras Online!..")
+    else:
+        cameraOnline = False
+        print("Cameras Offline!..")
+
+    while cameraOnline:
+        pir.wait_for_motion()
+        led.on()
+        print("Motion detected")
+        camera.capture('/home/pi/Desktop/image%s.jpg' %i)
+        led.off()
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    ToggleBot(True)
+
+@bot.message_handler(commands=['stop'])
+def send_welcome(message):
+	ToggleBot(False)
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, "Wrong Input")
+
+bot.polling()
