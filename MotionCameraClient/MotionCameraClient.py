7382e4900861cc25c92e5ec52f3e2ee6c9d33584
Paul Wenzlaff <Paulwenzlaff@gmail.com>
2020-07-02 09:20:46 +0200

    Starting to send photo via Telegram

--- MotionCameraClient/MotionCameraClient.py
+++ MotionCameraClient/MotionCameraClient.py
@@ -15,20 +15,16 @@ GPIO.setup(16,GPIO.IN)
 bot = telebot.TeleBot("886187441:AAFmuBkGdv4bDJHYaDQVXFWaePyYQic6Eko")
 chatID = 621572890
 
-makePictures = false
+cameraOnline = False
 
-def ToggleBot(value):
-    if value == True:
-        cameraOnline = True
-        print("Cameras Online!..")
-    else:
-        cameraOnline = False
-        print("Cameras Offline!..")
+def ToggleBot():
+    global cameraOnline
 
     while cameraOnline:
         if GPIO.input(16) == GPIO.HIGH:
             print("Motion detected...")
             camera.start_preview()
+            time.sleep(0.5)
             camera.capture('/home/pi/images/image.jpg')
             camera.stop_preview()
 
@@ -69,25 +65,29 @@ def ToggleBot(value):
             GPIO.output(18,GPIO.LOW)
             time.sleep(0.1)
 
+
+def SetCameraValue(value):
+    global cameraOnline
+    global botIsRunning
+
+    cameraOnline = value
+
+    if cameraOnline == True:
+        print("Cameras online!...")
+        ToggleBot()
+    else:
+        print("Cameras offline!...")
+
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    ToggleBot(True)
+    SetCameraValue(True)
 
 @bot.message_handler(commands=['stop'])
 def send_welcome(message):
-	ToggleBot(False)
-
-@bot.message_handler(commands=['deactivate'])
-def send_welcome(message):
-	camera.stop_preview()
-
-@bot.message_handler(commands=['activate'])
-def send_welcome(message):
-	camera.start_preview()
+	SetCameraValue(False)
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     bot.reply_to(message, "Wrong Input")
 
 bot.polling()
-print("Bot ready...")
