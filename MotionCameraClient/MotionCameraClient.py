aa95807d0e06fedb94e6fa0afc298bd2b768d9be
Facing-South <Paulwenzlaff@gmail.com>
2020-12-04 03:29:38 -0800

    code commented

--- MotionCameraClient/MotionCameraClient.py
+++ MotionCameraClient/MotionCameraClient.py
@@ -13,6 +13,7 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(18,GPIO.OUT)
 GPIO.setup(16,GPIO.IN)
 
+# read the telegram token and the group id from the token.txt
 f = open("token.txt", "r")
 lines = f.readlines()
 token = lines[0].strip()
@@ -21,8 +22,10 @@ groupId = lines[1].strip()
 print("Token: " + str(token))
 print("GroupID: " + str(groupId))
 
+# initialize the telegram bot
 bot = telebot.TeleBot(str(token))
 
+# starts the monitoring of the motion sensor and sends an image if the motion sensor reacts
 def ToggleBot():
     global cameraOnline
 
@@ -37,6 +40,7 @@ def ToggleBot():
             BlinkSlow(6)
             BlinkSlow(3)
 
+# let the led blink fast
 def BlinkFast(times):
     timesBlinked = 0
     while int(timesBlinked) != int(times):
@@ -46,6 +50,7 @@ def BlinkFast(times):
         time.sleep(0.1)
         timesBlinked = timesBlinked + 1
 
+# let the led blink slow
 def BlinkSlow(times):
     timesBlinked = 0
     while int(timesBlinked) != int(times):
@@ -55,6 +60,7 @@ def BlinkSlow(times):
         time.sleep(0.5)
         timesBlinked = timesBlinked + 1
 
+# initializes the camera and starts the monitoring if camera is online
 def SetCameraValue(value):
     global cameraOnline
     global botIsRunning
@@ -67,14 +73,17 @@ def SetCameraValue(value):
     else:
         print("Cameras offline!...")
 
+# command from telegram to start the monitoring
 @bot.message_handler(commands=['start'])
 def StartCamera(message):
     SetCameraValue(True)
 
+# command from telegram to stop the monitoring
 @bot.message_handler(commands=['stop'])
 def StopCamera(message):
 	SetCameraValue(False)
 
+# bot responds to the input of the user
 @bot.message_handler(func=lambda message: True)
 def EchoAll(message):
     bot.reply_to(message, "Wrong Input")
